d8dddbe84b8ab602578ce8579fe75170cdc2aa64
nomercy2009 <103402227+nomercy2009@users.noreply.github.com>
2022-06-07 17:55:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -162,3 +162,11 @@ function paddleInCanvas(){
     mouseY =0;
   }  
 }
+
+  function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
